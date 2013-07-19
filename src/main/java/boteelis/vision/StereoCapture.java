4f67b047d4262f05f36ac7b49ed12e5470ad91f2
tlaukkan <tommi.s.e.laukkanen@gmail.com>
2013-07-19 22:49:46 +0300

    Tested capture.

--- src/main/java/boteelis/vision/StereoCapture.java
+++ src/main/java/boteelis/vision/StereoCapture.java
@@ -3,6 +3,7 @@ package boteelis.vision;
 import com.github.sarxos.webcam.Webcam;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.File;
 
 /**
@@ -15,8 +16,22 @@ import java.io.File;
 public class StereoCapture {
 
     public static void main(String[] args) throws Exception {
-        Webcam webcam = Webcam.getDefault();
-        webcam.open();
-        ImageIO.write(webcam.getImage(), "PNG", new File("target/capture.png"));
+
+        Webcam webcam1 = Webcam.getWebcams().get(0);
+        Webcam webcam2 = Webcam.getWebcams().get(1);
+        webcam1.setViewSize(new Dimension(640, 480));
+        webcam1.open();
+        webcam2.setViewSize(new Dimension(640, 480));
+        webcam2.open();
+
+        for (int i = 0; i < 100; i++) {
+            long startTimeMillis = System.currentTimeMillis();
+            webcam1.getImage();
+            webcam2.getImage();
+            System.out.println(i + ") " + (System.currentTimeMillis() - startTimeMillis) + " ms.");
+        }
+
+        webcam1.close();
+        webcam2.close();
     }
 }
